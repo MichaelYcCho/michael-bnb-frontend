aa3020ae57a76fb3c6e135e1535f943adb2c597c
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2023-01-04 23:34:35 +0900

    feat: Init Setting

---
+++ src/routes/NotFound.tsx
@@ -0,0 +1,17 @@
+import { Button, Heading, Text, VStack } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+
+export default function NotFound() {
+  return (
+        // vstack is a vertical stack
+    <VStack bg="gray.100" justifyContent={"center"} minH="100vh">
+        <Heading>404</Heading>
+      <Text>It seems that you're lost.</Text>
+        <Link to="/">
+            <Button colorScheme={"red"} variant={"link"}>
+                Go home &rarr;
+            </Button>
+      </Link>
+    </VStack>
+  );
+}
