255c11a4ea5ddaaac8780702d6ae00d19383978b
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2023-01-05 00:04:28 +0900

    feat: Header

--- src/routes/NotFound.tsx
+++ src/routes/NotFound.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 export default function NotFound() {
   return (
-        // vstack is a vertical stack
+        // vstack is a vertical stack (Flex Container), MinHeight = minH
     <VStack bg="gray.100" justifyContent={"center"} minH="100vh">
         <Heading>404</Heading>
       <Text>It seems that you're lost.</Text>
