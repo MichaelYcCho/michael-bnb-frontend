11de67cbbfc104fb62d3ed0de404638fe1b17944
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2023-01-09 23:48:58 +0900

    feat: Fetching Room

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,12 +1,37 @@
-import {
-  Grid, Skeleton,
-} from "@chakra-ui/react";
+import { Box, Grid, HStack, Skeleton, SkeletonText } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
 import Room from "../components/Room";
 import RoomSkeleton from "../components/RoomSkeleton";
 
-//base -> 모바일 버전, lg -> 데스크탑 버전 (차크라는 모바일을 우선적용)
+interface IPhoto {
+  pk: string;
+  file: string;
+  description: string;
+}
+
+interface IRoom {
+  pk: number;
+  name: string;
+  country: string;
+  city: string;
+  price: number;
+  rating: number;
+  is_owner: boolean;
+  photos: IPhoto[];
+}
 
 export default function Home() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [rooms, setRooms] = useState<IRoom[]>([]);
+  const fetchRooms = async () => {
+    const response = await fetch("http://127.0.0.1:8000/api/v1/rooms/");
+    const json = await response.json();
+    setRooms(json);
+    setIsLoading(false);
+  };
+  useEffect(() => {
+    fetchRooms();
+  }, []);
   return (
     <Grid
       mt={10}
@@ -24,21 +49,30 @@ export default function Home() {
         "2xl": "repeat(5, 1fr)",
       }}
     >
-
-      <>
-        <RoomSkeleton />
-        <RoomSkeleton />
-        <RoomSkeleton />
-        <RoomSkeleton />
-        <RoomSkeleton />
-        <RoomSkeleton />
-        <RoomSkeleton />
-        <RoomSkeleton />
-        <RoomSkeleton />
-        <RoomSkeleton />
-      </>
-
-
+      {isLoading ? (
+        <>
+          <RoomSkeleton />
+          <RoomSkeleton />
+          <RoomSkeleton />
+          <RoomSkeleton />
+          <RoomSkeleton />
+          <RoomSkeleton />
+          <RoomSkeleton />
+          <RoomSkeleton />
+          <RoomSkeleton />
+          <RoomSkeleton />
+        </>
+      ) : null}
+      {rooms.map((room) => (
+        <Room
+          imageUrl={room.photos[0].file}
+          name={room.name}
+          rating={room.rating}
+          city={room.city}
+          country={room.country}
+          price={room.price}
+        />
+      ))}
     </Grid>
   );
 }
