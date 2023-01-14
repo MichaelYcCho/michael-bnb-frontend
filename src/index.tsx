c280332a090a8640de61cd233c9a3cea27600f75
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2023-01-14 21:11:12 +0900

    feat: Axios

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,4 @@
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
-import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "./router";
@@ -12,12 +11,12 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <QueryClientProvider client={client}>
-      <ChakraProvider theme={theme}>
-        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-        <RouterProvider router={router} />
-      </ChakraProvider>
-    </QueryClientProvider>
-  </React.StrictMode>
+
+  <QueryClientProvider client={client}>
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      <RouterProvider router={router} />
+    </ChakraProvider>
+  </QueryClientProvider>
+
 );
