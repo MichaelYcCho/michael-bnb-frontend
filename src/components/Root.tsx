cbee53d6973addc037bb41830d03bdec0ad070ba
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2023-01-16 22:10:38 +0900

    feat: DevTool and QueryKeys

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -1,12 +1,14 @@
 import { Box } from "@chakra-ui/react";
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 export default function Root() {
   return (
     <Box>
       <Header />
       <Outlet />
+      <ReactQueryDevtools />
     </Box>
   );
 }
