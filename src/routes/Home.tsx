09e383791e121ad9d94e7fc9ea22cad006eaf527
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2023-01-17 22:13:06 +0900

    feat: Photo Grid

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -3,26 +3,11 @@ import { useQuery } from "@tanstack/react-query";
 import { getRooms } from "../api";
 import Room from "../components/Room";
 import RoomSkeleton from "../components/RoomSkeleton";
+import { IRoomList } from "../types";
 
-interface IPhoto {
-  pk: string;
-  file: string;
-  description: string;
-}
-
-interface IRoom {
-  pk: number;
-  name: string;
-  country: string;
-  city: string;
-  price: number;
-  rating: number;
-  is_owner: boolean;
-  photos: IPhoto[];
-}
 
 export default function Home() {
-  const { isLoading, data } = useQuery<IRoom[]>(["rooms"], getRooms);
+  const { isLoading, data } = useQuery<IRoomList[]>(["rooms"], getRooms);
   return (
     <Grid
       mt={10}
