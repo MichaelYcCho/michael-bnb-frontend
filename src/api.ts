529ca1af371eb18e319cc17315d609c89d40f052
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2023-01-18 22:59:34 +0900

    feat: LogOut

--- src/api.ts
+++ src/api.ts
@@ -1,3 +1,4 @@
+import Cookie from "js-cookie";
 import { QueryFunctionContext } from "@tanstack/react-query";
 import axios from "axios";
 
@@ -23,3 +24,12 @@ export const getRoomReviews = ({ queryKey }: QueryFunctionContext) => {
 
 export const getMe = () =>
   instance.get(`users/me`).then((response) => response.data);
+
+export const logOut = () =>
+  instance
+  .post(`users/log-out`, null, {
+    headers: {
+      "X-CSRFToken": Cookie.get("csrftoken") || "",
+    },
+  })
+  .then((response) => response.data);
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,15 +6,23 @@ import {
   HStack,
   IconButton,
   LightMode,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
   Stack,
   useColorMode,
   useColorModeValue,
   useDisclosure,
+  useToast,
 } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 import LoginModal from "./LoginModal";
 import SignUpModal from "./SignUpModal";
 import useUser from "../lib/useUser";
+import { logOut } from "../api";
+import { useQueryClient } from "@tanstack/react-query";
+
 
 export default function Header() {
   const { userLoading, isLoggedIn, user } = useUser();
@@ -31,6 +39,24 @@ export default function Header() {
   const { toggleColorMode } = useColorMode();
   const logoColor = useColorModeValue("red.500", "red.200");
   const Icon = useColorModeValue(FaMoon, FaSun);
+  const toast = useToast();
+  const queryClient = useQueryClient();
+
+  const onLogOut = async () => {
+    const toastId = toast({
+      title: "Login out...",
+      description: "Sad to see you go...",
+      status: "loading",
+      position: "bottom-right",
+    });
+    await logOut();
+        queryClient.refetchQueries(["me"]);
+        toast.update(toastId, {
+          status: "success",
+          title: "Done!",
+          description: "See you later!",
+        });
+  };
   return (
     <Stack
       justifyContent={"space-between"}
@@ -70,7 +96,14 @@ export default function Header() {
               </LightMode>
             </>
           ) : (
-            <Avatar name={user?.name} src={user?.avatar} size={"md"} />
+            <Menu>
+              <MenuButton>
+                <Avatar name={user?.name} src={user?.avatar} size={"md"} />
+              </MenuButton>
+              <MenuList>
+                <MenuItem onClick={onLogOut}>Log out</MenuItem>
+              </MenuList>
+            </Menu>
           )
         ) : null}
       </HStack>
