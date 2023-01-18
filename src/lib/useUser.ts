f550c4d84e91dda3668b19f407836ae28e3fcd32
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2023-01-18 22:17:54 +0900

    feat: Credentials

--- src/lib/useUser.ts
+++ src/lib/useUser.ts
@@ -1,9 +1,11 @@
 import { useQuery } from "@tanstack/react-query";
 import { getMe } from "../api";
+import { IUser } from "../types";
 
 export default function useUser() {
-  const { isLoading, data, isError } = useQuery(["me"], getMe, {
-    retry: false,
+
+    const { isLoading, data, isError } = useQuery<IUser>(["me"], getMe, {
+        retry: false,
   });
   return {
     userLoading: isLoading,
@@ -11,3 +13,4 @@ export default function useUser() {
     isLoggedIn: !isError,
   };
 }
+
