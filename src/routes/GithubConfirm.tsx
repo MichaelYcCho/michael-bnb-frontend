11d4d12babb3e546b3f0fe6411235bc61cac5d96
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2023-01-22 09:10:12 +0900

    feat: After Github Login

--- src/routes/GithubConfirm.tsx
+++ src/routes/GithubConfirm.tsx
@@ -1,4 +1,4 @@
-import { Heading, Spinner, Text, VStack } from "@chakra-ui/react";
+import { Heading, Spinner, Text, VStack, useToast } from "@chakra-ui/react";
 import { useQueryClient } from "@tanstack/react-query";
 import { useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
@@ -8,11 +8,24 @@ export default function GithubConfirm() {
     // useLocation -> 현재 위치를 알려줌 (path_name)
     // search는 query string을 알려줌
   const { search } = useLocation();
+  const toast = useToast();
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
   const confirmLogin = async () => {
     const params = new URLSearchParams(search);
     const code = params.get("code");
     if (code) {
-      await githubLogIn(code);
+      const status = await githubLogIn(code);
+      if (status === 200) {
+        toast({
+          status: "success",
+          title: "Welcome!",
+          position: "bottom-right",
+          description: "Happy to have you back!",
+        });
+        queryClient.refetchQueries(["me"]);
+        navigate("/");
+      }
     }
   };
   useEffect(() => {
