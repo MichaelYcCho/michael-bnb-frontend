ec90fb430dedce3ee9674cecb2157e84e971d244
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2023-02-01 00:12:34 +0900

    feat: Upload Room Category, Amenities

--- src/api.ts
+++ src/api.ts
@@ -87,4 +87,8 @@ export const usernameLogIn = ({
     }
   );
 
-    
+export const getAmenities = () =>
+  instance.get(`rooms/amenities`).then((response) => response.data);
+
+export const getCategories = () =>
+  instance.get(`categories`).then((response) => response.data);
--- src/routes/UploadRoom.tsx
+++ src/routes/UploadRoom.tsx
@@ -1,10 +1,12 @@
 import {
     Box,
+    Button,
     Checkbox,
     Container,
     FormControl,
     FormHelperText,
     FormLabel,
+    Grid,
     Heading,
     Input,
     InputGroup,
@@ -13,13 +15,17 @@ import {
     Textarea,
     VStack,
   } from "@chakra-ui/react";
-  import { FaBed, FaMoneyBill, FaToilet} from "react-icons/fa";
+  import { useQuery } from "@tanstack/react-query";
+  import { FaBed, FaMoneyBill, FaToilet, FaUsb } from "react-icons/fa";
+  import { getAmenities, getCategories } from "../api";
   import useHostOnlyPage from "../components/HostOnlyPage";
   import ProtectedPage from "../components/ProtectedPage";
-  
+  import { IAmenity, ICategory } from "../types";
 
 export default function UploadRoom() {
     useHostOnlyPage();
+    const { data: amenities, isLoading: isAmenitiesLoading } = useQuery<IAmenity[]>(["amenities"], getAmenities);
+    const { data: categories, isLoading: isCategoriesLoading } = useQuery<ICategory[]>(["categories"], getCategories);
     return (
         <ProtectedPage>
         <Box
@@ -32,7 +38,7 @@ export default function UploadRoom() {
         >
             <Container>
             <Heading textAlign={"center"}>Upload Room</Heading>
-            <VStack spacing={5} as="form" mt={5}>
+            <VStack spacing={10} as="form" mt={5}>
                 <FormControl>
                     <FormLabel>Name</FormLabel>
                     <Input required type="text" />
@@ -98,6 +104,36 @@ export default function UploadRoom() {
                         What kind of room are you renting?
                     </FormHelperText>
                 </FormControl>
+
+                <FormControl>
+                    <FormLabel>Category</FormLabel>
+                    <Select placeholder="Choose a kind">
+                        {categories?.map((category) => (
+                        <option key={category.pk} value={category.pk}>
+                            {category.name}
+                        </option>
+                        ))}
+                    </Select>
+                    <FormHelperText>
+                        What category describes your room?
+                    </FormHelperText>
+                </FormControl>
+
+                <FormControl>
+                    <FormLabel>Amenities</FormLabel>
+                    <Grid templateColumns={"1fr 1fr"} gap={5}>
+                        {amenities?.map((amenity) => (
+                        <Box key={amenity.pk}>
+                            <Checkbox>{amenity.name}</Checkbox>
+                            <FormHelperText>{amenity.description}</FormHelperText>
+                        </Box>
+                        ))}
+                    </Grid>
+                </FormControl>
+
+                <Button colorScheme={"red"} size="lg" w="100%">
+                Upload Room
+                </Button>
             </VStack>
             </Container>
         </Box>
