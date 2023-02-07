0aa244f42c1e846fe35a7ca7b91de19040887879
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2023-02-07 20:30:08 +0900

    feat: GitHub with Mutation

--- src/routes/GithubConfirm.tsx
+++ src/routes/GithubConfirm.tsx
@@ -1,5 +1,5 @@
 import { Heading, Spinner, Text, VStack, useToast } from "@chakra-ui/react";
-import { useQueryClient } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { githubLogIn } from "../api";
@@ -10,24 +10,41 @@ export default function GithubConfirm() {
   const { search } = useLocation();
   const toast = useToast();
   const queryClient = useQueryClient();
+  const mutation = useMutation(githubLogIn, {
+    onMutate: () => {
+      console.log("start mutate");
+    },
+    onSuccess: () => {
+      toast({
+        title: "Welcome!",
+        status: "success",
+      });
+      queryClient.refetchQueries(["me"]);
+      navigate("/");
+    },
+    onError: (error) => {
+      console.log(`error : ${error}`);
+    },
+  });
   const navigate = useNavigate();
   const confirmLogin = async () => {
-    const params = new URLSearchParams(search);
-    const code = params.get("code");
-    if (code) {
-      const status = await githubLogIn(code);
-      if (status === 200) {
-        toast({
-          status: "success",
-          title: "Welcome!",
-          position: "bottom-right",
-          description: "Happy to have you back!",
-        });
-        queryClient.refetchQueries(["me"]);
-        navigate("/");
-      }
-    }
-  };
+  const params = new URLSearchParams(search);
+  const code = params.get("code");
+  if (code) {
+    mutation.mutate(code);
+    // const status = await githubLogIn(code);
+    // if (status === 200) {
+    //   toast({
+    //     status: "success",
+    //     title: "Welcome!",
+    //     position: "bottom-right",
+    //     description: "Happy to have you back!",
+    //   });
+    //   queryClient.refetchQueries(["me"]);
+    //   navigate("/");
+    // }
+  }
+};
   useEffect(() => {
     confirmLogin();
   }, []);
